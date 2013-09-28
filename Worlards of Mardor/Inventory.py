cafdc70992eb3c8f3c5c41d3f3aba54a48a93c87
dHelmgren <devincollege@gmail.com>
2013-09-27 23:48:17 -0700

    Updated the code for the Inventory class

--- Worlards of Mardor/Inventory.py
+++ Worlards of Mardor/Inventory.py
@@ -5,9 +5,27 @@ __author__ = 'Devin'
 #Description: This file holds all the relevant information about a player's inventory
 #
 #Variables:
-#
+#   blood - the amount of currency currently held by the
+#   favor - the amount of favor the player holds with their deity
+#   unitList[] - the units owned by the player
+#   foodstuffs - the amount of food that the player can give to their livestock and workers
+#   maxUnits - the number of plots that the user has right now
 #
 
 class Inventory(object):
 
+    ##
+    #__init__
+    #Description: initialized the player's inventory
+    #
+    ##
     def __init__(self):
+        blood = 300
+        favor = 0.5
+        foodstuffs = 10
+        unitList = []
+        maxUnits = 6
+
+    #this will need getter and setter methods for each variable
+
+    
